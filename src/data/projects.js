cdb5d24fc227e32f81fb631835bc67ec0dd1e5e6
Dat Tran <datitran@gmail.com>
2019-01-30 18:44:49 +0100

    Add more projects and change main text.

--- src/data/projects.js
+++ src/data/projects.js
@@ -18,5 +18,45 @@ export default [
         "ownerLogo": "idealo",
         "projectId": "image-super-resolution",
         "language": "python"
+    },
+    {
+        "title": "nvidia-docker-keras",
+        "description": "Workflow that shows how to train neural networks on EC2 instances with GPU support and compares training times to CPUs.",
+        "videoUrl": "",
+        "ownerId": "idealo",
+        "ownerUrl": "http://idealo.de",
+        "ownerLogo": "idealo",
+        "projectId": "nvidia-docker-keras",
+        "language": "python"
+    },
+    {
+        "title": "terraform-emr-pyspark",
+        "description": "Quickstart PySpark with Anaconda on AWS/EMR using Terraform.",
+        "videoUrl": "",
+        "ownerId": "idealo",
+        "ownerUrl": "http://idealo.de",
+        "ownerLogo": "idealo",
+        "projectId": "terraform-emr-pyspark",
+        "language": "hcl"
+    },
+    {
+        "title": "falcon-prediction-app",
+        "description": "Simple Machine Learning Web API Example with Falcon.",
+        "videoUrl": "",
+        "ownerId": "idealo",
+        "ownerUrl": "http://idealo.de",
+        "ownerLogo": "idealo",
+        "projectId": "falcon-prediction-app",
+        "language": "python"
+    },
+    {
+        "title": "jenkins-ci",
+        "description": "Minimal example to setup a Jenkins-CI pipeline for data science projects on OpenShift in a couple of minutes.",
+        "videoUrl": "",
+        "ownerId": "idealo",
+        "ownerUrl": "http://idealo.de",
+        "ownerLogo": "idealo",
+        "projectId": "jenkins-ci",
+        "language": "dockerfile"
     }
 ]
