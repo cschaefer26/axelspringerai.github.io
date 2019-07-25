8cf6bfb83b58b07c3945f4866f1f77ab0f0cc723
datitran <datitran@gmail.com>
2019-07-25 09:52:05 +0200

    Change color of footer to white.

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Footer.scss';
 
-import Socials from '../../components/Socials/Socials'
+var FontAwesome = require('react-fontawesome');
 
 export default (props) => {
   return (
     <footer className='footer__container'>
       <div className='footer__content'>
-        <Socials />
+        <p>
+            <a href='https://www.xing.com/companies/axelspringerse' className='footer__item'><FontAwesome name='xing' /></a>
+            <a href='https://de.linkedin.com/company/axel-springer-se' className='footer__item'><FontAwesome name='linkedin' /></a>
+            <a href='https://www.instagram.com/axel_springer_karriere/' className='footer__item'><FontAwesome name='instagram' /></a>
+            <a href='https://www.facebook.com/AxelSpringerKarriere/' className='footer__item'><FontAwesome name='facebook' /></a>
+        </p>
         <p className='footer__links'>
           <a href='https://www.axelspringer.com/de/impressum'>Impressum</a>
           <a href='https://www.axelspringer.com/de/datenschutzerklaerung'>Hinweise zum Datenschutz</a>
