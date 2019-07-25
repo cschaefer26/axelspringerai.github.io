33adc1fe2cdc7954b7dd5853517fb758f86d5d42
datitran <datitran@gmail.com>
2019-07-25 09:02:07 +0200

    Add team section to page.

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 
 import Navigation from '../Navigation/Navigation'
 import Hero from '../Hero/Hero'
+import Team from '../Team/Team'
 import Projects from '../Projects/Projects'
 import Jobs from '../Jobs/Jobs'
 import Footer from '../Footer/Footer'
@@ -16,6 +17,7 @@ export default (props) => {
         <article className='app__container'>
             <Navigation />
             <Hero />
+            <Team />
             <Projects />
             <Jobs />
             <Footer />
