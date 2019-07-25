b83e710ac7f0eee1b5aa28ccdca7c7e5be699026
datitran <datitran@gmail.com>
2019-07-25 09:32:56 +0200

    Add AS font.

--- webpack.config.js
+++ webpack.config.js
@@ -73,6 +73,18 @@ module.exports = (env) => {
                         'postcss-loader',
                         'sass-loader'
                     ]
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [
+                      {
+                        loader: 'file-loader',
+                        options: {
+                          name: '[name].[ext]',
+                          outputPath: 'fonts/'
+                        }
+                      }
+                    ]
                 }
             ],
         },
