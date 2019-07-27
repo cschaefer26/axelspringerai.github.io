899d146dc8a80cd854a360066e8054fc721da76e
datitran <datitran@gmail.com>
2019-07-27 14:27:01 +0200

    Use twitter instead of xing.

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -8,8 +8,8 @@ export default (props) => {
     <footer className='footer__container'>
       <div className='footer__content'>
         <p>
-            <a href='https://www.xing.com/companies/axelspringerse' className='footer__item'><FontAwesome name='xing' /></a>
-            <a href='https://de.linkedin.com/company/axel-springer-se' className='footer__item'><FontAwesome name='linkedin' /></a>
+            <a href='https://twitter.com/axelspringer_EN' className='footer__item'><FontAwesome name='twitter' /></a>
+            <a href='https://www.linkedin.com/company/axel-springer-se/' className='footer__item'><FontAwesome name='linkedin' /></a>
             <a href='https://www.instagram.com/axel_springer_karriere/' className='footer__item'><FontAwesome name='instagram' /></a>
             <a href='https://www.facebook.com/AxelSpringerKarriere/' className='footer__item'><FontAwesome name='facebook' /></a>
         </p>
