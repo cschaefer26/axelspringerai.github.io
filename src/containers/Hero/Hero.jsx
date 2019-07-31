607da5886832e7056e76b6db762d349cc87c3d3f
datitran <datitran@gmail.com>
2019-07-31 19:59:33 +0200

    Rephrase main text a little bit and also put it some breaks.

--- src/containers/Hero/Hero.jsx
+++ src/containers/Hero/Hero.jsx
@@ -25,7 +25,13 @@ export default (props) => {
       </div>
       <p className='hero__text'>
         <h3>who we are<span className='hero__underscore'> _</span></h3>
-        <strong>Axel Springer AI</strong> is the artificial intelligence unit of <a href="https://www.axelspringer.com/" className='hero__links' target="_blank">Axel Springer SE</a>, one of the largest digital publishing house in Europe. We're also currently part of <a href="https://www.ideas-engineering.io/" className='hero__links' target="_blank">Axel Springer Ideas Engineering</a>, the innovation unit of Axel Springer. Our mission is to make AI accessible to everyone and hence drive innovations. We deliver end-to-end machine learning projects and conduct state-of-the-art AI research. Our research focus ranges from traditional machine learning to modern deep learning. We mainly employ open-source technologies for our work. We also like to give talks on conferences and share our knowledge frequently. If you'd like to co-operate with us then please <a href="mailto:dat.tran@axelspringer.com" className='hero__links'>contact us</a>!
+        <strong>Axel Springer AI</strong> is the artificial intelligence unit of <a href="https://www.axelspringer.com/" className='hero__links' target="_blank">Axel Springer SE</a>, one of the largest digital publishing house in Europe. We're also currently part of <a href="https://www.ideas-engineering.io/" className='hero__links' target="_blank">Axel Springer Ideas Engineering</a>, the innovation unit of Axel Springer. 
+        
+        <br /><br />
+        Our mission is to make AI accessible to everyone within Axel Springer and hence drive innovations. We deliver end-to-end machine learning projects and conduct state-of-the-art AI research. Our research focus is mainly on Computer Vision and NLP problems. 
+            
+        <br /><br />            
+        We mainly employ open-source technologies for our work. We also like to give talks on conferences and share our knowledge frequently. If you'd like to co-operate with us then please <a href="mailto:dat.tran@axelspringer.com" className='hero__links'>contact us</a>!
       </p >
     </header >
   );
