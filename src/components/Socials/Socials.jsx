eeef36564be4885741c57254ca3d4928848df04e
datitran <datitran@gmail.com>
2019-09-08 15:27:28 +0200

    Change viewpoint for hero and also change blog link to medium and linkedin to AS AI.

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -7,8 +7,8 @@ export default (props) => {
     return (
         <p className={'socials__container ' + props.className}>
             <a href='https://twitter.com/axelspringer_EN' className='socials__item'><FontAwesome name='twitter' /></a>
-            <a href='https://tech.axelspringer.com/' className='socials__item'><FontAwesome name='pencil' /></a>
-            <a href='https://www.linkedin.com/company/axel-springer-se/' className='socials__item'><FontAwesome name='linkedin' /></a>
+            <a href='https://medium.com/axel-springer-tech' className='socials__item'><FontAwesome name='medium' /></a>
+            <a href='https://www.linkedin.com/company/axel-springer-ai/about/' className='socials__item'><FontAwesome name='linkedin' /></a>
             <a href='https://www.instagram.com/axel_springer_karriere/' className='socials__item'><FontAwesome name='instagram' /></a>
             <a href='https://www.facebook.com/AxelSpringerKarriere/' className='socials__item'><FontAwesome name='facebook' /></a>
         </p>
